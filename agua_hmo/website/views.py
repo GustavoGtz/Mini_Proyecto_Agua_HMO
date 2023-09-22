987e3dca154885b73566f76a321878d0a14d8d60
Gustavo Gutierrez <gustavogtznav@gmail.com>
2023-09-21 20:08:06 -0700

    me quiero matar

--- agua_hmo/website/views.py
+++ agua_hmo/website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .models import Users, Debts
+import json
+
 
 
 # Create your views here.
@@ -13,12 +15,24 @@ def manage_users(request):
 
 def manage_payment_concept(request):
     if request.method == 'POST':
-        user_meter_number = int(request.POST.get('meter_number', 0))
-        searched_user = Users.objects.filter(meter_number=user_meter_number)
-        if searched_user.exists():
-            return render(request, 'user_manage_payment_concept.html', {'user' : searched_user.first, 'debts' : Debts.objects.filter(meter_number=user_meter_number)})
-        else:
-            return render(request, 'manage_payment_concept.html', {'error': True})
+        action = request.POST.get('action')
+        if action == 'find_user':
+            user_meter_number = int(request.POST.get('meter_number', 0))
+            searched_user = Users.objects.filter(meter_number=user_meter_number)
+            if searched_user.exists():
+                return render(request, 'user_manage_payment_concept.html', {'user' : searched_user.first, 'debts' : Debts.objects.filter(meter_number=user_meter_number)})
+            else:
+                return render(request, 'manage_payment_concept.html', {'error': True})
+        if action == 'create_ticket':
+            print("@AAAAAAAAFRawfaw")
+            print(request.POST.get('meter_number'))
+            user_meter_number = int(request.POST.get('meter_number', 0))
+            ticket_year = int(request.POST.get('year', 0))
+            ticket_month = int(request.POST.get('month', 0))
+            return render(request, 'ticket.html', {'user' : Users.objects.filter(meter_number=user_meter_number).first,
+                                                   'ticket' : Debts.objects.filter(meter_number=user_meter_number, year=ticket_year, month=ticket_month).first})
+
+        
     return render(request, 'manage_payment_concept.html', {'error': False})
 
 def createUser(request):
