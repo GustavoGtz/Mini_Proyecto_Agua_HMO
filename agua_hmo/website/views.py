c356832b782eae3156bb7bd0c151c74037deb3c8
victor-rios03 <victor_rios03@protonmail.com>
2023-09-22 10:15:56 -0700

    Usuarios

--- agua_hmo/website/views.py
+++ agua_hmo/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .models import Users, Debts
+from .forms import UserForm
 import json
 
 
@@ -35,14 +36,49 @@ def manage_payment_concept(request):
         
     return render(request, 'manage_payment_concept.html', {'error': False})
 
-def createUser(request):
-    form = UserCreationForm()
+def create_user(request):
+    form = UserForm()
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
-    return render(request, 'user_form.html', {"form": form})
+    context = {'form': form}
+    return render(request, 'user_form.html', context)
+
+def delete_user(request):
+    if request.method == 'POST':
+        user_meter_number = int(request.POST.get('meter_number', 0))
+        searched_user = Users.objects.filter(meter_number=user_meter_number)
+        if searched_user.exists():
+            searched_user.delete()
+    return render(request, 'manage_payment_concept.html', {'error': False})
+
+def update_user(request):
+    if request.method == 'POST':
+        user_meter_number = int(request.POST.get('meter_number', 0))
+        searched_user = Users.objects.filter(meter_number=user_meter_number)
+        if searched_user.exists():
+            searched_user.delete()
+        form = UserForm()
+        if request.method == 'POST':
+            form = UserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('home')
+        context = {'form': form}
+        return render(request, 'user_form.html', context)
+    return render(request, 'manage_payment_concept.html', {'error': False})
+
+def show_user(request):
+    if request.method == 'POST':
+        user_meter_number = int(request.POST.get('meter_number', 0))
+        searched_user = Users.objects.filter(meter_number=user_meter_number)
+        if searched_user.exists():
+            return render(request, 'user_info.html', {'user' : searched_user.first})
+        else:
+            return render(request, 'manage_payment_concept.html', {'error': True})
+    return render(request, 'manage_payment_concept.html', {'error': False})
 
 
 
