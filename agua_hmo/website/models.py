fc188a50b47c1f2e4ff8dd9de7f52f3814ae75e9
Gustavo Gutierrez <gustavogtznav@gmail.com>
2023-09-22 20:07:13 -0700

    Generacion automatica de cuotas, se arreglo la informacion del ticket y se pulieron algunos otros detallitos

--- agua_hmo/website/models.py
+++ agua_hmo/website/models.py
@@ -12,31 +12,30 @@ MAX_CHAR_LENGTH = 20
 
 class Users(models.Model):
     CONTRACT_TYPES = (
-        ('R', 'Residencial'),
-        ('C', 'Comercial'),
-        ('I', 'Industrial'),
+        ('Residencial', 'RESIDENCIAL'),
+        ('Comercial', 'COMERCIAL'),
+        ('Industrial', 'INDUSTRIAL'),
     )
-
+    
     meter_number = models.IntegerField(validators=[
             MinValueValidator(10000, "El número debe ser mayor o igual a 10000."),
             MaxValueValidator(99999, "El número debe ser menor o igual a 99999.")
-        ])
+        ], unique=True)
     user_name = models.CharField(max_length=MAX_CHAR_LENGTH)
     home_direction = models.CharField(max_length=MAX_CHAR_LENGTH)
-    contract_type = models.CharField(max_length=1, choices=CONTRACT_TYPES)
+    contract_type = models.CharField(max_length=12, choices=CONTRACT_TYPES)
     register_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return str(self.meter_number) + ' : ' + self.user_name
 
 class Concepts(models.Model):
-
     year = models.DecimalField(max_digits=4, decimal_places=0)
-    consumption_per_cubic = models.DecimalField(max_digits=10, decimal_places=2)
-    drainage_fee = models.DecimalField(max_digits=3, decimal_places=2)
-    sanitation = models.DecimalField(max_digits=3, decimal_places=2)
-    red_cross = models.DecimalField(max_digits=3, decimal_places=2)
-    firefighters = models.DecimalField(max_digits=2, decimal_places=2)
+    consumption_per_cubic = models.FloatField()
+    drainage_fee = models.FloatField()
+    sanitation = models.FloatField()
+    red_cross = models.FloatField()
+    firefighters = models.FloatField()
     
     def __str__(self):
         return str(self.year)
@@ -51,10 +50,8 @@ class Debts(models.Model):
     month = models.PositiveSmallIntegerField()
     cut = models.PositiveSmallIntegerField()
     water_usage_m3 = models.FloatField()
-    debt = models.FloatField()
     previous_debt = models.FloatField()
     total_month = models.FloatField()
-
 
     def __str__(self):
         return str(self.meter_number) + " : " + str(self.year) + "-" + str(self.month)
--- agua_hmo/website/views.py
+++ agua_hmo/website/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .models import Users, Debts, Concepts
 from .forms import UserForm
-import json
+from django.utils import timezone
+import random
 
 
-
-# Create your views here.
 
 def home(request):
     return render(request, 'home.html', {})
@@ -25,30 +24,98 @@ def manage_payment_concept(request):
             else:
                 return render(request, 'manage_payment_concept.html', {'error': True})
         if action == 'create_ticket':
-            print("@AAAAAAAAFRawfaw")
-            print(request.POST.get('meter_number'))
             user_meter_number = int(request.POST.get('meter_number', 0))
             ticket_year = int(request.POST.get('year', 0))
             ticket_month = int(request.POST.get('month', 0))
 
-            concep_info = Concepts.objects.filter(year=ticket_year)
-            debt_info = Debts.objects.filter(meter_number=user_meter_number, year=ticket_year, month=ticket_month)
+            concep_info = Concepts.objects.get(year=ticket_year)
+            debt_info = Debts.objects.get(meter_number=user_meter_number, year=ticket_year, month=ticket_month)
 
-            water_cons = concep_info.consumption_per_cubic * debt_info.water_usage_m3
-            drainage = concep_info.drainage_fee
-            sanitation = concep_info.sanitation
-            red_cross = concep_info.red_cross
-            firefighters = concep_info.firefighters
-            total_month = debt_info.total_month
+            water_cost = concep_info.consumption_per_cubic * debt_info.water_usage_m3
+            drainage_cost = concep_info.drainage_fee * water_cost
+            sanitation_cost = concep_info.sanitation * water_cost
+            red_cross_cost = concep_info.red_cross * water_cost
+            firefighters_cost = concep_info.firefighters * water_cost
+            total_month = water_cost + drainage_cost + sanitation_cost + red_cross_cost + firefighters_cost
             previous_debt = debt_info.previous_debt
-            total = water_cons + drainage + sanitation + red_cross + firefighters + total_month + previous_debt
-
+            total = total_month + previous_debt
 
             return render(request, 'ticket.html', {'user' : Users.objects.filter(meter_number=user_meter_number).first,
                                                    'ticket' : Debts.objects.filter(meter_number=user_meter_number, year=ticket_year, month=ticket_month).first,
-                                                   'concepts' : Concepts.objects.filter(year=ticket_year)})
-
+                                                   'water_cons' : water_cost,
+                                                   'drainage' : drainage_cost,
+                                                   'sanitation' : sanitation_cost,
+                                                   'red_cross' : red_cross_cost,
+                                                   'firefighters' : firefighters_cost,
+                                                   'total_month' : total_month,
+                                                   'previous_debt' : previous_debt,
+                                                   'total' : total
+                                                   })
         
+        if action == 'create_debt':
+            user_meter_number = int(request.POST.get('meter_number', 0))
+            searched_user = Users.objects.filter(meter_number=user_meter_number)
+            user_meter_number = int(request.POST.get('meter_number', 0))
+            user_history_of_debts = Debts.objects.filter(meter_number=user_meter_number)
+            if user_history_of_debts.exists():
+                user_last_debt = user_history_of_debts.latest('id')
+                random_water_usage = random.uniform(1, 10)
+                # Function to calculate the next year/month of the new debt
+                def calculate_next_debt_date(year, month):
+                    new_month = month + 1
+                    if(new_month >= 13) : 
+                        return(year+1, 1)
+                    else:
+                        return(year, new_month)
+                new_debt_date = calculate_next_debt_date(user_last_debt.year, user_last_debt.month)
+                concep_info = Concepts.objects.get(year=new_debt_date[0])
+                water_cost = concep_info.consumption_per_cubic * random_water_usage
+                total_month_full = (water_cost +
+                                         concep_info.drainage_fee * water_cost + 
+                                         concep_info.sanitation * water_cost +
+                                         concep_info.red_cross * water_cost +
+                                         concep_info.firefighters * water_cost +
+                                         user_last_debt.total_month)
+                # Generar pagina para cargar nuevo adeudeo.
+                new_debt = Debts(meter_number = user_meter_number,
+                                 year = new_debt_date[0],
+                                 month = new_debt_date[1],
+                                 cut = 20,
+                                 water_usage_m3 = round(random_water_usage, 3),
+                                 previous_debt = user_last_debt.total_month,
+                                 total_month = total_month_full)
+                new_debt.save()
+                return render(request, 'user_manage_payment_concept.html', {'user' : searched_user.first, 'debts' : Debts.objects.filter(meter_number=user_meter_number)})
+            else:
+                current_date = timezone.now().date()
+                current_year = current_date.year
+                current_month = current_date.month
+
+                concep_info = Concepts.objects.get(year=current_year)
+                random_water_usage = random.uniform(1, 10)
+                water_cost = concep_info.consumption_per_cubic * random_water_usage
+                
+                debt_info = Debts.objects.get(meter_number=user_meter_number, year=current_year, month=current_month)
+
+                total_month_full = (water_cost +
+                                         concep_info.drainage_fee * water_cost + 
+                                         concep_info.sanitation * water_cost +
+                                         concep_info.red_cross * water_cost +
+                                         concep_info.firefighters * water_cost)
+                                        
+                # Generar pagina para cargar un nuevo adeudo inicial.
+                new_debt = Debts(meter_number = user_meter_number,
+                                 year = current_year,
+                                 month = current_month,
+                                 cut = 20,
+                                 water_usage_m3 = round(random_water_usage, 3),
+                                 previous_debt = 0,
+                                 total_month = total_month_full)
+                new_debt.save()
+                return render(request, 'user_manage_payment_concept.html', {'user' : searched_user.first, 'debts' : Debts.objects.filter(meter_number=user_meter_number)})
+
+                
+            
     return render(request, 'manage_payment_concept.html', {'error': False})
 
 def create_user(request):
